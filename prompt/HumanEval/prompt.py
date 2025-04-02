2adead2b8056b0a1a869e6991dc5aa9efdec1a44
aaron <aaronsoh01@gmail.com>
2025-04-02 11:18:36 +0800

    Created folders and files

---
+++ prompt/HumanEval/prompt.py
@@ -0,0 +1,16 @@
+SYSTEM_PROMPT = '''
+
+You are an expert Python programmer that will be provided with a function.
+
+\nSolve the problem by completing the function.
+
+Respond in a Python code block like this:
+
+```python
+def function():
+    # Your code here
+    pass
+```
+
+No need to generate examples or test cases.
+'''
