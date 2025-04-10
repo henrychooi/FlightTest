cc0be757c6b7aa1b149e4ddbf4a382b4e1a76476
dumb-ish <69144044+dumb-ish@users.noreply.github.com>
2025-04-10 11:03:27 +0800

    Commit ARCChallenge evaluator

--- evaluator/ARCChallengeEvaluator.py
+++ evaluator/ARCChallengeEvaluator.py
@@ -5,21 +5,22 @@ import pandas as pd
 from tqdm import tqdm
 from jinja2 import Template
 import pyarrow
+from collections import Counter
 
-from .BaseEvaluator import BaseEvaluator
+from evaluator.BaseEvaluator import BaseEvaluator
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from utils.ARCChallenge.utils import create_prompt_list, read_parquet_file
+from utils.ARCChallenge.utils import create_parquet_file, calculate_accuracy
 
 
 class ARCChallengeEvaluator(BaseEvaluator):
 
     def __init__(self, model_path, data_path, output_path):
-        super().__init__(model_path, data_path, output_path)
+        super().__init__(model_path, data_path, None, output_path)
 
     def _load_model(self):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
   
-        tokenizer = AutoTokenizer.from_pretrained(self.model_path).to(device)
+        tokenizer = AutoTokenizer.from_pretrained(self.model_path)
         model = AutoModelForCausalLM.from_pretrained(
             self.model_path,
             device_map="auto",
@@ -28,10 +29,12 @@ class ARCChallengeEvaluator(BaseEvaluator):
         tokenizer.pad_token = tokenizer.eos_token
         model.eval()
 
+        print(f"Model loaded on {device}")
+
         return device, model, tokenizer
     
     def _prep_df(self):
-        df = pd.read_parquet_file(self.data_path, engine='pyarrow')
+        df = pd.read_parquet(self.data_path, engine='pyarrow')
         df['answerKey'] = df['answerKey'].astype('string')
         prompt_list = []
         answer_list = []
@@ -44,62 +47,69 @@ class ARCChallengeEvaluator(BaseEvaluator):
             answer_list.append(row["answerKey"])
 
         prompt_df = pd.DataFrame(list(zip(prompt_list, answer_list)), columns=['prompt','ground_truth'])
+
+        print(prompt_df.head())
+
+        print(f"Prompt DataFrame created with {len(prompt_df)} entries.")
+
         return prompt_df
-    
-    def _
-        
 
-    def evaluate_answer(answer_df):
-        for answer in answer_df:
-            match = re.search(r'####\s*([A-Da-d1-4])', answer)
-            if match:
-                first_char = match.group(1)
-                print("First alphanumeric character:", first_char)
-            else:
-                print("No match found.")
-
-    def _generate_answers(self):
+    def evaluate_model(self):
 
         device, model, tokenizer = self._load_model()
 
         prompt_df = self._prep_df()
 
+        response_list = []
+
         answer_list = []
 
-        for prompt in (pbar := tqdm(prompt_df.iterrows(), desc=f"Running inference", total=len(prompt_df))):
+        ground_truth_list = []
 
-            inputs = tokenizer(prompt, return_tensors="pt", padding=True).to(device)
+        for _, prompt_row in (pbar := tqdm(prompt_df.iterrows(), desc=f"Running inference", total=len(prompt_df))):
+
+            inputs = tokenizer(prompt_row['prompt'], return_tensors="pt", padding=True).to(device)
             attention_mask = inputs['attention_mask']
 
             with torch.no_grad():
                 outputs = model.generate(inputs['input_ids'],
                                         attention_mask=attention_mask,
-                                        max_new_tokens=22,
+                                        max_new_tokens=128,
                                         do_sample=True,
                                         pad_token_id=tokenizer.pad_token_id,
                                         eos_token_id=tokenizer.eos_token_id)
                 
             prompt_length = inputs['input_ids'].shape[1]
 
-            answer = tokenizer.decode(outputs[0][prompt_length:], skip_special_tokens=True)
+            response = tokenizer.decode(outputs[0][prompt_length:], skip_special_tokens=True)
             
+            response_list.append(response)
+
+            answers = re.findall(r'The best answer is ([A-Da-d1-4])\.', response)
+
+            answer = Counter(answers).most_common(1)[0][0]
+
             answer_list.append(answer)
+
+            ground_truth_list.append(prompt_row['ground_truth'])
+
+            current_accuracy = calculate_accuracy(answer_list, ground_truth_list)
 
             pbar.set_postfix(acc=f"{current_accuracy:.2%}")
 
         prompt_df.insert(1, 'answer', answer_list)
 
-        prompt_df.to_parquet(f'{self.output_path}.parquet')
+        prompt_df.insert(1, 'response', response_list)
 
+        output_path = f'{self.output_path}.parquet'
 
+        prompt_df.to_parquet(output_path, engine='pyarrow', index=False)
 
-        return sample_file
+        print(f"Final accuracy: {current_accuracy:.2%}")
+
+        print(f"Output saved to {output_path}")
+
     
-    def evaluate_answer(answer_df):
-        for answer in answer_df:
-            match = re.search(r'####\s*([A-Da-d1-4])', answer)
-            if match:
-                first_char = match.group(1)
-                print("First alphanumeric character:", first_char)
-            else:
-                print("No match found.")
+if __name__ == "__main__":
+    evaluator = ARCChallengeEvaluator(model_path="C:/Users/Augustine/models/meta-llama--Llama-3_2-1B-Instruct", data_path="C:/Users/Augustine/Documents/GitHub/FlightTest/data/ARCChallenge/test/test-00000-of-00001.parquet", output_path="C:/Users/Augustine/Documents/GitHub/FlightTest/output/ARCChallenge/meta-llama--Llama-3_2-1B-Instruct_results")
+    evaluator.evaluate_model()  
